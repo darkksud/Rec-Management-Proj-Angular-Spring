98c525562e961fc0ba8a7b8b5af42db8932aef7f
sd <darkksud@gmail.com>
2022-03-04 19:03:31 +0530

    manager dashboard changes

--- RecruitmentManagementSystem-development/src/app/manager-dashboard/manager-dashboard.component.ts
+++ RecruitmentManagementSystem-development/src/app/manager-dashboard/manager-dashboard.component.ts
@@ -1,8 +1,10 @@
 //import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import {Component, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-manager-dashboard',
@@ -20,56 +22,48 @@ export class ManagerDashboardComponent {
   @ViewChild(MatSort) sort: MatSort;
 
 
+  constructor(private http : HttpClient){
+    
+  }
+  li:any;
+  lis=[];
+  path = environment.backGroundUrl + "api/managerDashBoard/getAll";
+  ngOnInit(): void {
+    
+    this.http.get(this.path)
+    .subscribe(Response => {
+      
+     
+      this.li=Response;
+      const users: UserData[] = this.li;
+    this.dataSource = new MatTableDataSource(users);
+    this.ngAfterViewInit();
+    this.applyFilter("");
+    });
+    
+  }
+
   /**
    * Set the paginator and sort after the view init since this component will
    * be able to query its view for the initialized paginator and sort.
    */
-   ngAfterViewInit() {
+  ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-
-   }
-
-  constructor() {
-    // Create 100 users
-    const users: UserData[] = [];
-    for (let i = 1; i <= 10; i++) { users.push(createNewUser(i)); }
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
   }
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
 }
-  function createNewUser(sNo: number): UserData {
-    const name =
-        NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-        NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '';
-  
-    return {
-      sNo: sNo.toString(),
-      name: name,
-      phone:  Math.round(Math.random() * 100).toString(),
-      email: EMAILS[Math.round(Math.random() * (EMAILS.length - 1))],
-     
-    
-    };
-
-}
-
-
-/** Constants used to fill up our data base. */
-const NAMES = ['1', '2', '', '3', '7', '3',
-  '', '4', '', '', '', '',
-  '', '', '', '4', '7', '2', '0'];
-const SKILLS = [''];
-const PROJECTS = [''];
-const EMAILS = ['3'];
-
 
 export interface UserData {
-  sNo: string;
-  name: string;
-  phone: string;
-  email: string;
+  projectName: string;
+  manualTesting: number;
+  automationTesting: number;
+  php: number;
+  sac: number;
 
 }
